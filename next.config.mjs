d17f9dcc9519acd2d585feabb4be86a95ded5615
Jorge Hermes <91022739+jhermesn@users.noreply.github.com>
2025-02-17 18:07:40 -0300

    fixed assetPrefix path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+const repositoryName = "simpleCanvas";
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -6,8 +10,13 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   output: "export",
+  basePath: isProd ? `/${repositoryName}` : "",
+  assetPrefix: isProd ? `/${repositoryName}/` : "",
 }
 
 export default nextConfig
